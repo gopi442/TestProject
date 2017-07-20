dc6673669abfc2e71f1f6a2ae0c5f86ddf8f6b26
Gopal Gupta <gopalgupta@Gopals-MacBook-Air.local>
2017-07-21 00:22:34 +0530

    New Test Project Added

---
+++ TestProjectDemo/TestProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProjectDemo
+//
+//  Created by Gopal Gupta on 21/07/17.
+//  Copyright © 2017 XYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProjectDemo/TestProjectDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestProjectDemo
+//
+//  Created by Gopal Gupta on 21/07/17.
+//  Copyright © 2017 XYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+      @autoreleasepool {
+          return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+      }
+}
